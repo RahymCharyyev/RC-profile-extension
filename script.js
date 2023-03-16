33825ee330c7e8f092f831757a31d0f640eafa70
RahymCharyev <r569837@gmail.com>
2023-03-16 13:08:27 +0500

    ✨feature: New Commit

--- script.js
+++ script.js
@@ -19,8 +19,14 @@ const copyToClipboard = (str) => {
 };
 
 let buttonLn = document.querySelector("#button-ln");
+let buttonTelega = document.querySelector("#button-telegram");
+let buttonHabr = document.querySelector("#button-habr");
 let buttonInsta = document.querySelector("#button-insta");
 let buttonGithub = document.querySelector("#button-github");
+let buttonMerjenyol = document.querySelector("#button-merjenyol");
+let buttonEuroasia = document.querySelector("#button-euroasia");
+let buttonAsteroids = document.querySelector("#button-asteroids");
+let buttonSpaceX = document.querySelector("#button-spacex");
 let buttonBeauty = document.querySelector("#button-beauty");
 let buttonItstudio = document.querySelector("#button-itstudio");
 let buttonGlo = document.querySelector("#button-glo");
@@ -29,13 +35,33 @@ let buttonOrg = document.querySelector("#button-org");
 let buttonRace = document.querySelector("#button-race");
 let buttonTrvl = document.querySelector("#button-trvl");
 buttonLn.addEventListener("click", () =>
-  copyToClipboard("https://www.linkedin.com/in/rahym-charyyev-451b19214/")
+  copyToClipboard("https://www.linkedin.com/in/rahym-charyyev-rc/")
+);
+buttonTelega.addEventListener("click", () =>
+  copyToClipboard("https://t.me/RC_IT")
+);
+buttonHabr.addEventListener("click", () =>
+  copyToClipboard("https://career.habr.com/rahymcharyyev")
 );
 buttonInsta.addEventListener("click", () =>
   copyToClipboard("https://www.instagram.com/charyyev_rahym/")
 );
 buttonGithub.addEventListener("click", () =>
   copyToClipboard("https://github.com/RahymCharyyev")
+);
+buttonMerjenyol.addEventListener("click", () =>
+  copyToClipboard("https://merjenyol.com.tm/")
+);
+buttonAsteroids.addEventListener("click", () =>
+  copyToClipboard("https://rahymcharyyev.github.io/asteroids-app/")
+);
+buttonSpaceX.addEventListener("click", () =>
+  copyToClipboard(
+    "https://rahymcharyyev.github.io/SpaceX-Rocket-Launches-Timeline/"
+  )
+);
+buttonEuroasia.addEventListener("click", () =>
+  copyToClipboard("https://euroasia-tm.com/")
 );
 buttonBeauty.addEventListener("click", () =>
   copyToClipboard("https://rahymcharyyev.github.io/Beauty/")
